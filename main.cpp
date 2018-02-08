659b77adb63740f526b548a0f56b6987455c5135
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-02-08 17:01:58 +0800

    setup one

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <cmath>
 
 using std::endl;
 using std::cout;
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello CMake User" << endl;
+    cout << "Usage: "<< argv[0]  << " number" << endl;
+
+    double inputValue;
+    if(argc == 2)
+    {
+        inputValue = atof(argv[1]);
+    }
+    else
+    {
+        inputValue = 4;
+    }
+
+    double outputValue = sqrt(inputValue);
+    cout << "The square root of "<<  inputValue << " is " << outputValue << endl;
     return 0;
 }
