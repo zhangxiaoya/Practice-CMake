04369aa06e3b2ad35aa565c05f28290d7fe632eb
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-02-09 10:17:58 +0800

    Setup7

--- MathFunctions/MathFunctions.cpp
+++ MathFunctions/MathFunctions.cpp
@@ -7,6 +7,13 @@ using std::endl;
 
 double mysqrt(double A)
 {
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    cout << "Check Feature" << endl;
+    result = exp(log(x) * 0.5);
+    return result;
+#else
+
+
     cout << "This sqrt is come from customer function"<< endl;
     double x0 = A + 0.25, x1, xx = x0;
     for (;;)
@@ -18,4 +25,5 @@ double mysqrt(double A)
         x0 = x1;
     }
     return x1;
+#endif
 }
